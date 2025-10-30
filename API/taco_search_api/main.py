203a0a8e6bfbf4f689b0917e0fb9f20fd3b22115
julio-sa <julio.ccbeu@gmail.com>
2025-10-30 01:34:11 -0300

    update main.py

--- API/taco_search_api/main.py
+++ API/taco_search_api/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:4200",
         "https://dieti-backend.onrender.com",
-        ],
+        "https://dieti.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
