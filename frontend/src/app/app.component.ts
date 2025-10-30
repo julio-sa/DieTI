2c74d3fb87d2d42131a6a99488b4585bf311028d
julio-sa <157306928+julio-sa@users.noreply.github.com>
2025-10-30 14:07:50 -0300

    Update app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterModule
   ],
-  template: `<router-outlet></router-outlet>`,
+  template: `./app.component.html`,
 })
 export class AppComponent {
   online = true;
   title = 'frontend';
 
-  constructor() {
-    window.addEventListener('online', () => this.updateStatus());
-    window.addEventListener('offline', () => this.updateStatus());
-  }
+  constructor(private swUpdate: SwUpdate) {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe(async (event) => {
+        if (event.type === 'VERSION_READY') {
+          await this.swUpdate.activateUpdate();
+          document.location.reload();
+        }
+      });
+    }
 
   updateStatus() {
     this.online = navigator.onLine;
